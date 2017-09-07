14e6aa911bec0c71edc8e07577374335264c37dd
shuang <yangshuang_dev@163.com>
2017-09-08 00:32:46 +0800

    Index Buffer Object completed.
    Signed-off-by: shuang <yangshuang_dev@163.com>

--- tutorials/hellotriangle/hellotriangle.cpp
+++ tutorials/hellotriangle/hellotriangle.cpp
@@ -13,18 +13,32 @@ void HelloTriangle::feed()
 	float vertices[] = {
 		-0.5f, -0.5f, 0.0f,
 		0.5f, -0.5f, 0.0f,
-		0.0f,  0.5f, 0.0f
+		0.5f,  0.5f, 0.0f,
+		-0.5f,  0.5f, 0.0f
+	};
+
+	int indices[] = {
+		0,1,2,
+		2,3,0
 	};
 
 	//VAO
 	glGenVertexArrays(1, &_VAO);
 	glBindVertexArray(_VAO);
 
+	//VBO
 	GLuint VBO;
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	//EBO
+	GLuint EBO;
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	//Attr
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
@@ -104,7 +118,9 @@ void HelloTriangle::feed()
 void core::HelloTriangle::render()
 {
 	glBindVertexArray(_VAO);
-	glDrawArrays(GL_TRIANGLES, 0, 3);
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
 }
 
 int main()
